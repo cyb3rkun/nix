82025575f33a7034e8b7bcdeb186c7e626a43caf
cyb3rkun <cyb0rg@duck.com>
2025-10-17 19:40:59 +0200

    add cowsay

--- home.nix
+++ home.nix
@@ -20,10 +20,6 @@
 	# The home.packages option allows you to install Nix packages into your
 	# environment.
 	home.packages = [
-		# # Adds the 'hello' command to your environment. It prints a friendly
-		# # "Hello, world!" when run.
-		# pkgs.hello
-
 		# # It is sometimes useful to fine-tune packages, for example, by applying
 		# # overrides. You can do that directly here, just don't forget the
 		# # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -36,6 +32,7 @@
 		# (pkgs.writeShellScriptBin "my-hello" ''
 		#   echo "Hello, ${config.home.username}!"
 		# '')
+		pkgs.cowsay
 	];
 
 	# Home Manager is pretty good at managing dotfiles. The primary way to manage
